f3423ac351775e8eece159cb75604e228edb68ee
Linda Lin <linda84715@gmail.com>
2024-07-30 17:35:33 +0100

    Register ok

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -2,23 +2,23 @@ import { db } from "../db.js"; //改檔案名稱
 import bcrypt from "bcryptjs";
 
 export const register = (req, res) => {
-    //CHECK EXISTING USER
-    const q = "SELECT * FROM users WHERE email = ? OR username = ?";
-  
-    db.query(q, [req.body.email, req.body.username], (err, data) => {
+  //檢查是否已存在用戶
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists!");
+
+    //加密密碼並創建用戶
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users(`username`, `email`, `password`, `firstName`, `lastName`, `points`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash, req.body.firstName, req.body.lastName, req.body.points];
+
+    db.query(q, [values], (err, data) => {
       if (err) return res.status(500).json(err);
-      if (data.length) return res.status(409).json("User already exists!");
-  
-      //Hash the password and create a user
-      const salt = bcrypt.genSaltSync(10);
-      const hash = bcrypt.hashSync(req.body.password, salt);
-  
-      const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-      const values = [req.body.username, req.body.email, hash];
-  
-      db.query(q, [values], (err, data) => {
-        if (err) return res.status(500).json(err);
-        return res.status(200).json("User has been created.");
+      return res.status(200).json("User has been created.");
       });
     });
   };
